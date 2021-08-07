8ea2f451acb8bd6897ee559b1e3a53a35989cc23
Rob Taylor <roboncode@gmail.com>
2021-08-07 16:20:07 -0600

    init commit

---
+++ app.ts
@@ -0,0 +1,7 @@
+import { serve } from 'https://deno.land/std@0.103.0/http/server.ts'
+const server = serve({ port: 3000 })
+console.log(`Server running on: http://localhost:3000/`)
+
+for await (const req of server) {
+  req.respond({ status: 200, body: 'Hello, world!\n' })
+}
